2edb41f46b895250e1e108b5b7ce7f2bb6e17ec3
cgehuzi <mail.cgehuzi@gmail.com>
2019-11-28 18:05:02 +0300

    [ world ] - add script

--- js/scripts.js
+++ js/scripts.js
@@ -308,3 +308,59 @@ if (tabs_blocks) {
 	});
 }
 // END ------------------------
+
+// ====================================================
+// Реализация блока world-map
+// ====================================================
+
+// функция создания контента в баллунах
+const getBaloonContent = function(pin) {
+	return '<div class="world-map__baloon">' + '<a class="world-map__baloon-item" href="' + pin['href'] + '">' + '<img class="world-map__baloon-image" src="' + pin['image'] + '">' + '<div class="world-map__baloon-title">' + pin['title'] + '</div>' + '<div class="world-map__baloon-desc">' + pin['desc'] + '</div>' + '</a>' + '</div>';
+};
+
+const map_init = function(map_params) {
+	ymaps.ready(function() {
+		const myMap = new ymaps.Map(
+				map_params['id'], // находим блок, в котором создадим карту
+				{
+					center: map_params['mapCenter'], // координаты центральной точки на карте
+					zoom: map_params['mapZoom'], // увеличение карты
+					controls: []
+				},
+				{ searchControlProvider: 'yandex#search' }
+			),
+			objectManager = new ymaps.ObjectManager({
+				// Чтобы метки начали кластеризоваться, выставляем опцию.
+				clusterize: true,
+				// ObjectManager принимает те же опции, что и кластеризатор.
+				gridSize: 32,
+				clusterDisableClickZoom: false
+			});
+
+		myMap.controls.add('zoomControl', {
+			position: {
+				bottom: 'auto',
+				left: 'auto',
+				right: 40,
+				top: 40
+			}
+		});
+
+		// Чтобы задать опции одиночным объектам и кластерам,
+		// обратимся к дочерним коллекциям ObjectManager.
+		objectManager.objects.options.set({
+			iconLayout: 'default#image',
+			iconImageHref: 'assets/templates/zrobym/images/pin.png',
+			iconImageSize: [30, 70],
+			iconImageOffset: [-15, -37]
+		});
+		objectManager.clusters.options.set({
+			preset: 'islands#nightClusterIcons',
+			iconColor: '#000000'
+		});
+		myMap.geoObjects.add(objectManager);
+		myMap.behaviors.disable('scrollZoom');
+		objectManager.add(map_params['mapPins']);
+	});
+};
+// END ------------------------
