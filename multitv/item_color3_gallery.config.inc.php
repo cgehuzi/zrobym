da2cbee52c2ebd4fd336ea5fb6187adb69d00a05
cgehuzi <mail.cgehuzi@gmail.com>
2019-12-14 14:18:59 +0300

    [ furniture-item ] - add some blocks

---
+++ multitv/item_color3_gallery.config.inc.php
@@ -0,0 +1,14 @@
+<?php
+$settings['display'] = 'vertical';
+$settings['fields'] = array(
+    'thumb' => array(
+        'caption' => 'Thumbnail',
+        'type' => 'thumb',
+        'thumbof' => 'image'
+    ),
+    'image' => array(
+        'caption' => 'Изображение<br><span class="comment">Мин. ширина: 1000 px</span>',
+        'type' => 'image',
+        'width' => '30%'
+    )
+);
