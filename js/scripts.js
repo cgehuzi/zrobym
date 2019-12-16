15126b7e35b797ed988cd674730b2499dbea4af9
cgehuzi <mail.cgehuzi@gmail.com>
2019-12-17 00:22:22 +0300

    [ tabs ] - fix tab_was

--- js/scripts.js
+++ js/scripts.js
@@ -321,12 +321,12 @@ if (tab_blocks) {
 		const tab_was_selector = sessionStorage.getItem('tab_was');
 		if (tab_was_selector) {
 			const tab_button_was = document.querySelector('[data-tab-open="' + tab_was_selector + '"]');
-			if (tab_button_was) {
-				tab_button_was.click();
-				if (window.location.href.split('?').length > 1) {
-					if (window.location.href.split('?')[1].indexOf('-page=') > 0) {
-						$('html, body').animate({ scrollTop: $(tab_buttons[0]).offset().top }, 600);
-					}
+			if (tab_button_was && window.location.href.split('?').length > 1) {
+				if (window.location.href.split('?')[1].indexOf('-page=') > 0) {
+					tab_button_was.click();
+					$('html, body').animate({ scrollTop: $(tab_buttons[0]).offset().top }, 600);
+				} else {
+					tab_buttons[0].click();
 				}
 			} else {
 				tab_buttons[0].click();
