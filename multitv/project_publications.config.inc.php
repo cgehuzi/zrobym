7d3b8b03b1da5acc9a787bdef478edcaa0d7ae24
cgehuzi <mail.cgehuzi@gmail.com>
2020-06-21 22:12:14 +0300

    [ project-pub ] - add styles

---
+++ multitv/project_publications.config.inc.php
@@ -0,0 +1,19 @@
+<?php
+$settings['display'] = 'vertical';
+$settings['fields'] = array(
+    'thumb' => array(
+        'caption' => 'Thumbnail',
+        'type' => 'thumb',
+        'thumbof' => 'image'
+    ),
+    'image' => array(
+        'caption' => 'Логотип площадки<br><span class="comment">Размер: 1500 x 780 px</span>',
+        'type' => 'image',
+        'width' => '30%'
+    ),
+    'link' => array(
+        'caption' => 'Ссылка на публикацию',
+        'type' => 'text',
+        'width' => '60%'
+    )
+);
